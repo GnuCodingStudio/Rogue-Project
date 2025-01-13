db243ab6459ea92ed8fe93dff8b69485f4ff5e58
Olivier PEREZ <olivier@olivierperez.fr>
2025-01-13 20:56:28 +0100

    feat(debug): open debug level from game console

--- scenes/console/GlobalGameConsole.gd
+++ scenes/console/GlobalGameConsole.gd
@@ -4,7 +4,11 @@ extends GameConsole
 
 func _open_debug_level() -> void:
 <<<<<<< HEAD
+<<<<<<< HEAD
 	SceneTransition.change_scene("res://scenes/levels/debug/DebugLevel.tscn")
 =======
 	SceneTransition.change_scene("res://scenes/menus/main/MainMenu.tscn")
 >>>>>>> 905b5e4 (feat(console): make the open-debug-level command triggerable from everywhere)
+=======
+	SceneTransition.change_scene("res://scenes/levels/debug/DebugLevel.tscn")
+>>>>>>> f30998f (feat(debug): open debug level from game console)
