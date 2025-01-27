74965f356dc22f1501d1fe05a6bf0840f63ad720
Anthony MARQUET <anthony.marquet21@gmail.com>
2025-01-27 17:33:03 +0100

    feat: remove useless global variable and add dynamic player name

--- scenes/levels/world.gd
+++ scenes/levels/world.gd
@@ -11,18 +11,18 @@ func _unhandled_input(event):
 		SceneTransition.change_scene("res://scenes/menus/lobby/LobbyMenu.tscn")
 		
 func _ready() -> void:
+	if not multiplayer.is_server(): return
 	load_server_for_host()
 		
 func load_server_for_host():
-	if GlobalVariables.server_is_loaded:
-		multiplayer.peer_connected.connect(add_player)
-		multiplayer.peer_disconnected.connect(remove_player)
-		_players_spawn_node = get_tree().get_current_scene().get_node("PlayerSpawnPoint")
-		add_player(multiplayer.get_unique_id())
-		GlobalVariables.server_is_loaded = false
+	print("Host load the server")
+	multiplayer.peer_connected.connect(add_player)
+	multiplayer.peer_disconnected.connect(remove_player)
+	_players_spawn_node = get_tree().get_current_scene().get_node("PlayerSpawnPoint")
+	add_player(multiplayer.get_unique_id())
 
 func add_player(peer_id):
-	print("add_player", peer_id)
+	print("Player %s join the game", peer_id)
 	var player_scene = preload("res://objects/actors/rogue-player/rogue-player.tscn")
 	var player = player_scene.instantiate()
 	player.name = str(peer_id)
