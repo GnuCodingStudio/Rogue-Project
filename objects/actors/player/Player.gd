86af576f1701c5bf6f89f35b3f7bebf775a36c46
PopsIDX <etienne.idoux@gmail.com>
2025-01-31 08:37:07 +0100

    feat(weapon): Add weapon system and gun

--- objects/actors/player/Player.gd
+++ objects/actors/player/Player.gd
@@ -1,7 +1,7 @@
 extends Actor
 class_name Player
 
-@export var projectile: PackedScene
+@export var current_weapon: Weapon
 
 func _input(event):
 	if event is InputEventKey:
@@ -9,18 +9,18 @@ func _input(event):
 		moving_direction = direction.normalized()
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
-			attack(get_global_mouse_position())
+			attack()
 
 func apply_attack(force: int) -> void:
 	prints("Attacked with force", force)
 
-func attack(target_position: Vector2):
-	if not projectile: return
+
+func attack():
+	if not current_weapon: return
 	
-	var projectile_instance = projectile.instantiate()
-	var direction = global_position.direction_to(target_position)
-	projectile_instance.global_position = global_position + (direction * 20)
+	var mouseCoords = get_global_mouse_position()
+	var direction = global_position.direction_to(mouseCoords).normalized()
 	
-	projectile_instance.targeted_direction = direction
-	
-	get_tree().current_scene.add_child(projectile_instance)
+	var attack_scene = current_weapon.initTo(direction)
+	attack_scene.global_position = global_position + (direction * 50)
+	get_tree().current_scene.add_child(attack_scene)
--- objects/actors/projectile/Projectile.gd
+++ objects/actors/projectile/Projectile.gd
@@ -1,6 +1,20 @@
-extends Actor
-class_name Projectile
+extends Area2D
+class_name Attack
 
 var targeted_direction: Vector2
 var distance_traveled: float = 0.0
-@export var max_range: int = 100
+
+var range: float
+var damage: float
+	
+func init(_damage, _range, _direction):
+	damage = _damage
+	range = _range
+	targeted_direction = _direction
+
+
+func _on_body_entered(body: Node) -> void:
+	if not body is Mob: return
+	
+	body.queue_free()
+	queue_free()
