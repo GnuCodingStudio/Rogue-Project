511ac49c9a61db042fdf5dae4aa90e060221f0c3
PopsIDX <etienne.idoux@gmail.com>
2025-02-12 08:27:23 +0100

    fix naming & remove useless signal

--- scenes/menus/weapon/WeaponChoice.gd
+++ scenes/menus/weapon/WeaponChoice.gd
@@ -3,7 +3,7 @@ extends Control
 ## TODO: this could be given dynamically when we have a boat scene
 @export var weapons: Array[Weapon] = []; 
 
-@onready var weaponsContainer: HBoxContainer = %WeaponsContainer
+@onready var weapons_container: HBoxContainer = %WeaponsContainer
 var weapon_choice_button: PackedScene = load('res://scenes/menus/weapon/button/WeaponChoiceButton.tscn')
 
 func _ready() -> void:
@@ -11,9 +11,4 @@ func _ready() -> void:
 		var button = weapon_choice_button.instantiate()
 		button.init(weapon)
 		
-		weaponsContainer.add_child(button)
-
-func _button_pressed(weaponSelected: Weapon):
-	StoreManager.player_weapon = weaponSelected
-	print("Weapon selected is ", weaponSelected.name, " : ", weaponSelected)
-	SceneTransition.change_scene("res://scenes/levels/debug/DebugLevel.tscn")
+		weapons_container.add_child(button)
