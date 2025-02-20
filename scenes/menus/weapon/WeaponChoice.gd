e5e7663bc35c2830363565f9ad086ce7e125655c
Anthony MARQUET <anthonymarquet21@gmail.com>
2025-02-20 14:27:12 +0100

    feat(multiplayer): sync navigation to the island (world)

--- scenes/menus/weapon/WeaponChoice.gd
+++ scenes/menus/weapon/WeaponChoice.gd
@@ -1,5 +1,6 @@
 extends Control
 
+
 ## TODO: this could be given dynamically when we have a boat scene
 @export var weapons: Array[Weapon] = []; 
 
@@ -7,8 +8,18 @@ extends Control
 var weapon_choice_button: PackedScene = load('res://scenes/menus/weapon/button/WeaponChoiceButton.tscn')
 
 func _ready() -> void:
+	
 	for weapon in weapons:
 		var button = weapon_choice_button.instantiate()
 		button.init(weapon)
 		
 		weapons_container.add_child(button)
+		button.player_choose_weapon.connect(_players_is_ready)
+
+func _players_is_ready():
+	if !multiplayer.is_server(): return
+	_load_island.rpc()
+
+@rpc("call_local")
+func _load_island() -> void:
+	SceneTransition.change_scene("res://scenes/levels/islands/island.tscn")
