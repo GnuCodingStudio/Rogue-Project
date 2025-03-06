a81d4cd818ec339226dfc6640139c69cbf118b1f
Audrey Gentili <audrey.gentili38210@gmail.com>
2025-03-06 14:23:26 +0100

    feat: save current island and boat position on map (#51)

--- scenes/map/IslandMarker.gd
+++ scenes/map/IslandMarker.gd
@@ -3,12 +3,20 @@ extends Node2D
 
 signal click(island_marker: IslandMarker)
 
-@export var id: String
+@export var id: int
 @export var enabled: bool = true:
 	set(value): 
 		enabled = value
 		%Clickable.disabled = !value
 
+func _ready():
+	if !multiplayer.is_server():
+		%Clickable.queue_free()
+
 func _on_click():
 	if (enabled):
 		click.emit(self)
+		
+func disable():
+	enabled = false
+	modulate = Color.DIM_GRAY
--- services/managers/store/StoreManager.gd
+++ services/managers/store/StoreManager.gd
@@ -1,3 +1,4 @@
 extends Node2D
 
 var player_weapon: Weapon
+var current_island: int = -1
