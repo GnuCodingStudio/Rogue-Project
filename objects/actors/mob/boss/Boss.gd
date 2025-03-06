5662ee9d0868e7ccf5887c645d3c617471c482a4
Audrey Gentili <audrey.gentili38210@gmail.com>
2025-03-06 14:23:26 +0100

    feat(boss): control when Actor can change the animation based on current state

--- objects/actors/mob/boss/Boss.gd
+++ objects/actors/mob/boss/Boss.gd
@@ -2,6 +2,9 @@ class_name Boss extends Mob
 
 @export var players_node: Node
 
+@onready var states: States = $States
+@onready var follow: Follow = $States/Follow
+
 var players: Array[Player] = []
 
 func _ready() -> void:
@@ -21,3 +24,7 @@ func _sort_by_distance(a: Node2D, b: Node2D) -> bool:
 	var distance_to_a = a.global_position.distance_to(self.global_position)
 	var distance_to_b = b.global_position.distance_to(self.global_position)
 	return (distance_to_a < distance_to_b)
+
+func _update_animation() -> void:
+	if states.current_state == follow:
+		super._update_animation()
