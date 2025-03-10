2105b841e31f4e6b6ec920397c7f38b2e1c3541c
Olivier PEREZ <olivier@olivierperez.fr>
2025-03-10 22:52:23 +0100

    feat(boss): make boss attack player when is close to him

---
+++ objects/actors/mob/boss/states/Attack.gd
@@ -0,0 +1,22 @@
+class_name Attack extends State
+
+@onready var animated_sprite: AnimatedSprite2D = %AnimatedSprite
+@onready var _boss: Boss = owner
+@onready var states: States = $".."
+@onready var follow: Follow = $"../Follow"
+
+func enter() -> void:
+	super.enter()
+	attack()
+
+func attack() -> void:
+	var direction = _boss._direction_name()
+	animated_sprite.play("Attack" + direction)
+	await animated_sprite.animation_finished
+	attack()
+
+func transition() -> void:
+	super.transition()
+	var distance_to_player = _boss.global_position.distance_to(_boss.get_targeted_player().global_position)
+	if distance_to_player > 150:
+		states.change_state(follow)
