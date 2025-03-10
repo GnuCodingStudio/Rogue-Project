d291630ad4321d3659224cb25cf1c953f60f379d
Olivier PEREZ <olivier@olivierperez.fr>
2025-03-10 22:52:23 +0100

    feat(boss): make boss attack player when is close to him

--- objects/actors/mob/boss/Boss.gd
+++ objects/actors/mob/boss/Boss.gd
@@ -9,3 +9,15 @@ func _ready() -> void:
 		if node is Player:
 			players.push_back(node)
 	prints("Boss found players:", players)
+
+func get_targeted_player() -> Player:
+	if players == null: return null
+	if players.is_empty(): return null
+
+	players.sort_custom(_sort_by_distance)
+	return players[0]
+
+func _sort_by_distance(a: Node2D, b: Node2D) -> bool:
+	var distance_to_a = a.global_position.distance_to(self.global_position)
+	var distance_to_b = b.global_position.distance_to(self.global_position)
+	return (distance_to_a < distance_to_b)
