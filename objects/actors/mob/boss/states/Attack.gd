b2aaa42fa6d3b90ec24018a18878d1e3d7671e2e
Olivier PEREZ <olivier@olivierperez.fr>
2025-03-11 13:41:37 +0100

    feat(boss): fix some animations

--- objects/actors/mob/boss/states/Attack.gd
+++ objects/actors/mob/boss/states/Attack.gd
@@ -4,6 +4,7 @@ class_name Attack extends State
 @onready var _boss: Boss = owner
 @onready var states: States = $".."
 @onready var follow: Follow = $"../Follow"
+@onready var idle: Idle = $"../Idle"
 
 func enter() -> void:
 	super.enter()
@@ -11,12 +12,19 @@ func enter() -> void:
 
 func attack() -> void:
 	var direction = _boss._direction_name()
-	animated_sprite.play("Attack" + direction)
+	var attack_animation = "Attack" + direction
+	prints("attack_animation", attack_animation)
+	animated_sprite.play(attack_animation)
 	await animated_sprite.animation_finished
 	attack()
 
 func transition() -> void:
 	super.transition()
-	var distance_to_player = _boss.global_position.distance_to(_boss.get_targeted_player().global_position)
-	if distance_to_player > 150:
+	var player = _boss.get_targeted_player()
+	if not player:
+		states.change_state(idle)
+		return
+	
+	var distance_to_player = _boss.global_position.distance_to(player.global_position)
+	if distance_to_player > 80:
 		states.change_state(follow)
