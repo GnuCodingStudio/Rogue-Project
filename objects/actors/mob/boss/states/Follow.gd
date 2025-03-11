d68087fc5f1ab87b7b4b6f09184268690672be6c
Olivier PEREZ <olivier@olivierperez.fr>
2025-03-11 13:41:37 +0100

    feat(boss): fix some animations

--- objects/actors/mob/boss/states/Follow.gd
+++ objects/actors/mob/boss/states/Follow.gd
@@ -3,13 +3,28 @@ class_name Follow extends State
 @onready var _boss: Boss = owner
 @onready var states: States = $".."
 @onready var attack: State = $"../Attack"
+@onready var idle: Idle = $"../Idle"
+
+var _following : bool = false
+
+func enter() -> void:
+	super.enter()
+	_following = true
 
 func _physics_process(delta: float) -> void:
 	super._physics_process(delta)
-	_boss.moving_direction = _boss.get_targeted_player().global_position - global_position
+	if _following:
+		_boss.moving_direction = _boss.get_targeted_player().global_position - global_position
 
 func transition() -> void:
 	super.transition()
-	var distance_to_player = _boss.global_position.distance_to(_boss.get_targeted_player().global_position)
-	if distance_to_player < 100:
+	var player = _boss.get_targeted_player()
+	if not player:
+		states.change_state(idle)
+		return
+		
+	var distance_to_player = _boss.global_position.distance_to(player.global_position)
+	if distance_to_player < 60:
+		_following = false
+		_boss.moving_direction = Vector2.ZERO
 		states.change_state(attack)
