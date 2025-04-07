c0598ca90f266eaa9cce24cf93666dc888d8028e
Olivier PEREZ <olivier@olivierperez.fr>
2025-04-07 13:52:51 +0200

    feat(multiplayer): let players be ready or not before game starts

--- scenes/menus/lobby/waiting/WaitingPlayerPanel.gd
+++ scenes/menus/lobby/waiting/WaitingPlayerPanel.gd
@@ -1,13 +1,34 @@
 class_name WaitingPlayerPanel
 extends PanelContainer
 
+@onready var texture_rect: TextureRect = %TextureRect
 @onready var name_label: Label = %NameLabel
 
-var _player: PlayerData
+const _CAPTAIN_ICON = preload("res://scenes/menus/lobby/waiting/captain.png")
+const _READY_ICON = preload("res://scenes/menus/lobby/waiting/ready.png")
+const _WAITING_ICON = preload("res://scenes/menus/lobby/waiting/waiting.png")
 
-func init(player: PlayerData) -> void:
+var _WAITING_COLOR = Color(1., 1., 1., .3)
+var _READY_COLOR = Color.WHITE
+
+var _player: PlayerData
+var _is_ready: bool
+
+func init(player: PlayerData, is_ready: bool) -> void:
 	_player = player
+	_is_ready = is_ready
 
 func _ready() -> void:
 	assert(_player != null, "The init function must be called first")
-	%NameLabel.text = _player.pseudo
+	name_label.text = _player.pseudo
+	if _player.is_server():
+		texture_rect.texture = _CAPTAIN_ICON
+	else:
+		if _is_ready:
+			texture_rect.texture = _READY_ICON
+			texture_rect.self_modulate = _READY_COLOR
+			self_modulate = _READY_COLOR
+		else:
+			texture_rect.texture = _WAITING_ICON
+			texture_rect.self_modulate = _WAITING_COLOR
+			self_modulate = _WAITING_COLOR
--- scenes/menus/lobby/waiting/WaitingRoom.gd
+++ scenes/menus/lobby/waiting/WaitingRoom.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var play_button: Button = %StartButton
 @onready var players: GridContainer = %Players
 
+var _ready_players: Array[int] = []
+
 const WAITING_PLAYER_PANEL = preload("res://scenes/menus/lobby/waiting/WaitingPlayerPanel.tscn")
 
 func _ready() -> void:
@@ -10,31 +12,44 @@ func _ready() -> void:
 		play_button.text = "WAITING_START_BUTTON"
 	else:
 		play_button.text = "WAITING_READY_BUTTON"
+		play_button.toggle_mode = true
 	MultiplayerManager.player_list_changed.connect(_on_players_changed)
 	_on_players_changed()
 
 func _on_players_changed() -> void:
+	var all_players_ready := true
 	for c in players.get_children(): c.queue_free()
-	for player in MultiplayerManager.get_players():
+	
+	var players_list = MultiplayerManager.get_players()
+	for player in players_list:
 		var player_panel: WaitingPlayerPanel = WAITING_PLAYER_PANEL.instantiate()
-		player_panel.init(player)
+		var is_ready = _ready_players.any(func (id): return id == player.id)
+		player_panel.init(player, is_ready)
 		players.add_child(player_panel)
-	# TODO Instancier des WaitingPlayerPanel
+		all_players_ready = all_players_ready and (is_ready or player.is_server())
+	
+	if multiplayer.is_server():
+		play_button.disabled = !all_players_ready || players_list.size() <= 1
 
 func _on_start_button_pressed() -> void:
 	if multiplayer.is_server():
 		start_game.rpc()
 	else:
-		set_ready.rpc()
+		var is_ready = play_button.button_pressed
+		play_button.release_focus()
+		set_ready.rpc(is_ready)
 
 @rpc("call_local", "reliable")
 func start_game() -> void:
 	SceneTransition.change_scene("res://scenes/map/Map.tscn")
 
-@rpc("any_peer", "reliable")
-func set_ready() -> void:
+@rpc("any_peer", "call_local", "reliable")
+func set_ready(ready: bool) -> void:
 	prints(multiplayer.get_remote_sender_id(), "is ready")
-	# TODO Marquer le joueur comme prêt
+	if ready:
+		_ready_players.push_back(multiplayer.get_remote_sender_id())
+	else:
+		_ready_players.erase(multiplayer.get_remote_sender_id())
 	_on_players_changed()
 
 # TODO Garder ça ?
